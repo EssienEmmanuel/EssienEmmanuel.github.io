02d4f0680900083399abbdf7ad0af1b4fd208e8d
Essien Emmanuel <essienemma79@gmail.com>
2025-04-25 14:09:25 -0700

    form val

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const readMore = document.querySelector(".read-more");
+const readMoreBtn = document.querySelector(".read-more");
+const aboutMe = document.querySelector(".about-me-p");
+
+const fName = document.querySelector(".fname");
+const mail = document.querySelector(".email");
+const message = document.querySelector(".message");
+
+const msgButton = document.querySelector("#msg-button");
+
+const errorHead1 = document.querySelector(".error-heading1");
+const errorHead2 = document.querySelector(".error-heading2");
 
 function regard() {
   const greeting = document.querySelector(".greeting");
@@ -22,28 +33,95 @@ function regard() {
 
 regard();
 
-// // let isReadMore = false;
-
-// readMore.addEventListener("click", () => {
-//   const hideText = document.querySelector(".hide-text");
-
-//   if (!isReadMore) {
-//     readMore.innerHTML = "Read Less";
-//     hideText.style.display = "block";
-//     isReadMore = true;
-//   } else {
-//     readMore.innerHTML = "Read More";
-//     hideText.style.display = "none";
-//     isReadMore = false;
-//   }
-// });
-
-const readMoreBtn = document.querySelector(".read-more");
-const aboutMe = document.querySelector(".about-me-p");
-
 readMoreBtn.addEventListener("click", () => {
   aboutMe.classList.toggle("expanded");
   readMoreBtn.textContent = aboutMe.classList.contains("expanded")
     ? "Read Less"
     : "Read More";
+});
+
+// --------------------------------------Event Listeners
+
+msgButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const fNameValue = document.querySelector(".fname").value;
+  const mailValue = document.querySelector(".email").value;
+  const messageValue = document.querySelector(".message").value;
+
+  const mail = document.querySelector(".email");
+
+  const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+  const msgStatus = document.querySelector(".msg-status");
+  const fNameError = document.querySelector(".fname-error");
+  const emailError = document.querySelector(".email-error");
+  const validEmail = document.querySelector(".valid-email-error");
+  const msgError = document.querySelector(".msg-error");
+
+  let isValid = true;
+
+  if (fNameValue === "" && mailValue === "" && messageValue === "") {
+    fName.classList.add("error");
+    mail.classList.add("error");
+    message.classList.add("error");
+  } else if (fNameValue === "") {
+    fName.classList.add("error");
+    fNameError.style.display = "block";
+
+    isValid = false;
+  } else if (mailValue === "") {
+    mail.classList.add("error");
+    emailError.style.display = "block";
+    isValid = false;
+  } else if (messageValue === "") {
+    message.classList.add("error");
+    msgError.style.display = "block";
+
+    isValid = false;
+  }
+
+  if (!mailValue.match(emailPattern)) {
+    mail.style.border = "1px solid red";
+    // validEmail.style.display = "block";
+    isValid = false;
+  } else {
+    mail.style.border = "1px solid green";
+    validEmail.style.display = "none";
+  }
+
+  if (isValid) {
+    msgStatus.style.display = "block";
+    msgStatus.style.color = "green";
+    msgStatus.textContent = "Message Sent!";
+  }
+
+  fName.addEventListener("click", () => {
+    fName.classList.remove("error");
+    // mail.classList.remove("error");
+    // message.classList.remove("error");
+
+    fNameError.style.display = "none";
+    emailError.style.display = "none";
+    msgError.style.display = "none";
+  });
+
+  mail.addEventListener("click", () => {
+    // fName.classList.remove("error");
+    mail.classList.remove("error");
+    // message.classList.remove("error");
+
+    // fNameError.style.display = "none";
+    // emailError.style.display = "none";
+    msgError.style.display = "none";
+  });
+
+  message.addEventListener("click", () => {
+    // fName.classList.remove("error");
+    // mail.classList.remove("error");
+    message.classList.remove("error");
+
+    fNameError.style.display = "none";
+    emailError.style.display = "none";
+    msgError.style.display = "none";
+  });
 });
